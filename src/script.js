d7b68e21d09cf06a1a67797949f6d347e7a86c97
idaism <ida.bjarney@outlook.com>
2022-11-15 19:00:41 +0000

    started js logic

--- src/script.js
+++ src/script.js
@@ -1,19 +1,114 @@
 import './styles/styles.scss';
 
-const displayNumber = document.querySelector('.number-input');
-const inputHistory = document.querySelector('.input-history');
+let displayNumber = document.querySelector('.number-input-value');
+const inputHistoryEl = document.querySelector('.input-history');
 const numberButtons = Array.from(document.querySelectorAll('.number'));
 const plusBtn = document.querySelector('.plus-btn');
+const minusBtn = document.querySelector('.minus-btn');
+const multiplyBtn = document.querySelector('.multiply-btn');
+const divideBtn = document.querySelector('.divide-btn');
+const equalsBtn = document.querySelector('.equals-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const eraseBtn = document.querySelector('.erase-btn');
+
+let storedNumber = '';
+let inputHistory = [];
+let storedNumberArr = [];
+
+console.log(storedNumberArr)
+
+defaultState();
+
+// event listeners -----------------------------------------
+
+clearBtn.addEventListener('click', defaultState);
+
+eraseBtn.addEventListener('click', function() {
+  if (storedNumber > 1) {
+    let slicedNumber = storedNumber.slice(0, -1);
+    storedNumber = slicedNumber;
+    displayNumber.textContent = slicedNumber;
+  } else {
+    let slicedNumber = storedNumber.slice(0, -1);
+    storedNumber = slicedNumber;
+    displayNumber.textContent = 0;
+  }
+});
+
+plusBtn.addEventListener('click', plusOperator);
+minusBtn.addEventListener('click', minusOperator);
+multiplyBtn.addEventListener('click', multiplyOperator);
+divideBtn.addEventListener('click', divideOperator);
+equalsBtn.addEventListener('click', equalsOperator);
+
+// foreach loop --------------------------------------------
 
 numberButtons.forEach((number, index) => {
   number.addEventListener('click', function() {
-    console.log(number, index)
+    const buttonNumber = number.textContent;
+    storedNumber += buttonNumber;
+    displayNumber.textContent = storedNumber;
   })
 });
 
+// functions -----------------------------------------------
+
+
+
+function pushToArr(storedNumber) {
+  storedNumberArr.push(storedNumber);
+  // if(storedNumber.length < 2) {
+  //   storedNumberArr.push(storedNumber);
+  // } else {
+  //   storedNumberArr.shift();
+  //   storedNumberArr.push(storedNumber);
+  // }
+}
+
+function defaultState() {
+  inputHistoryEl.textContent = '';
+  clearDisplayNumber();
+};
+
+function clearDisplayNumber() {
+  displayNumber.textContent = 0;
+  storedNumber = '';
+};
 
 function plusOperator() {
-  let number = numberButtons.value;
-  
-}
+  inputHistory.push(storedNumber);
+  inputHistory.push('+');
+  inputHistoryEl.textContent = inputHistory.join(' ');
+  clearDisplayNumber();
+  storedNumberArr.push(storedNumber);
+};
+
+function minusOperator() {
+  inputHistory.push(storedNumber);
+  inputHistory.push('-');
+  inputHistoryEl.textContent = inputHistory.join(' ');
+  clearDisplayNumber();
+};
+
+function multiplyOperator() {
+  inputHistory.push(storedNumber);
+  inputHistory.push('×');
+  inputHistoryEl.textContent = inputHistory.join(' ');
+  clearDisplayNumber();
+};
+
+function divideOperator() {
+  inputHistory.push(storedNumber);
+  inputHistory.push('÷');
+  inputHistoryEl.textContent = inputHistory.join(' ');
+  clearDisplayNumber();
+};
+
+function equalsOperator() {
+  inputHistory.push(storedNumber);
+  inputHistory.push('=');
+  inputHistoryEl.textContent = inputHistory.join(' ');
+  clearDisplayNumber();
+};
+
 
